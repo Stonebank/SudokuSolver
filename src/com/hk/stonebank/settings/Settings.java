0eaa263606dc20a55bef88455650ce541c0effa2
Hassan K <hassan_99@live.dk>
2022-12-11 14:03:10 +0100

    Added settings for easier customization of various actions

--- src/com/hk/stonebank/settings/Settings.java
+++ src/com/hk/stonebank/settings/Settings.java
@@ -6,7 +6,10 @@ public class Settings {
 
     public static final boolean DEBUG_MODE = false;
 
+    public static final int SUDOKU_BOARD_SIZE = 9;
+
     public static final File BOARD_IMAGE = new File("./resources/board/board.png");
     public static final File BOARD_IMAGE_OUTPUT = new File("./resources/board/board_output.png");
+    public static final File BOARD_CELL_IMAGE_OUTPUT = new File("./resources/board/cells/");
 
 }
