9e1f7d57271a0c828dddbab7d437e3cd430c8132
Hassan K <hassan_99@live.dk>
2022-12-11 13:55:16 +0100

    Added settings for easier customization of various actions

---
+++ src/com/hk/stonebank/settings/Settings.java
@@ -0,0 +1,12 @@
+package com.hk.stonebank.settings;
+
+import java.io.File;
+
+public class Settings {
+
+    public static final boolean DEBUG_MODE = false;
+
+    public static final File BOARD_IMAGE = new File("./resources/board/board.png");
+    public static final File BOARD_IMAGE_OUTPUT = new File("./resources/board/board_output.png");
+
+}
