b9e7071a99906cc08944efa16f107543f97abaee
Hassan K <hassan_99@live.dk>
2022-12-11 13:54:26 +0100

    Initial commit

---
+++ src/com/hk/stonebank/Launch.java
@@ -0,0 +1,19 @@
+package com.hk.stonebank;
+
+import com.hk.stonebank.image.BoardDetection;
+import com.hk.stonebank.settings.Settings;
+import org.opencv.core.Core;
+
+public class Launch {
+
+    public static void main(String[] args) {
+
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+        // Board detection. This will attempt to detect the sudoku board in the image and crop the board and the cells of the image.
+        BoardDetection boardDetection = new BoardDetection(Settings.BOARD_IMAGE);
+        boardDetection.detect();
+
+    }
+
+}
