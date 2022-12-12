67b1f0cea1e108de1bdff9900a47f487b128d206
Hassan K <hassan_99@live.dk>
2022-12-12 23:07:09 +0100

    Added a new path for board cropped

--- src/com/hk/stonebank/settings/Settings.java
+++ src/com/hk/stonebank/settings/Settings.java
@@ -4,15 +4,16 @@ import java.io.File;
 
 public class Settings {
 
-    public static final boolean DEBUG_MODE = false;
+    public static final boolean DEBUG_MODE = true;
 
     public static final int SUDOKU_BOARD_SIZE = 9;
 
     public static final File BOARD_IMAGE = new File("./resources/board/board.png");
     public static final File BOARD_IMAGE_OUTPUT = new File("./resources/board/board_output.png");
+    public static final File BOARD_IMAGE_OUTPUT_CROPPED = new File("./resources/board/board_output_cropped.png");
     public static final File BOARD_CELL_IMAGE_OUTPUT = new File("./resources/board/cells/");
 
-    public static final File TESSERACT_TRAINED_DATA = new File("./resources/tesseract/model/");
+    public static final File TESSERACT_TRAINED_DATA = new File("./resources/tesseract/model/digits");
     public static final String TESSERACT_DPI = "300";
 
 }
