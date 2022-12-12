dd79d8ba74a6894fab2e92588749f015d31830fd
Hassan K <hassan_99@live.dk>
2022-12-12 23:06:54 +0100

    Added SudokuBoard with backtracking solution algorithm

---
+++ src/com/hk/stonebank/board/SudokuBoard.java
@@ -0,0 +1,77 @@
+package com.hk.stonebank.board;
+
+public class SudokuBoard {
+
+    private final int[][] board;
+    private final int size;
+
+    public SudokuBoard(int[][] board, int size) {
+        this.board = board;
+        this.size = size;
+    }
+
+    public void displayBoard() {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(" " + board[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public boolean canSolve() {
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                if (board[row][col] == 0) {
+                    for (int num = 1; num <= size; num++) {
+                        if (isAccepted(row, col, num)) {
+                            board[row][col] = num;
+                            if (canSolve())
+                                return true;
+                            else
+                                board[row][col] = 0;
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean inRow(int row, int num) {
+        for (int col = 0; col < size; col++)
+            if (board[row][col] == num)
+                return true;
+        return false;
+    }
+
+    private boolean inCol(int col, int num) {
+        for (int row = 0; row < size; row++)
+            if (board[row][col] == num)
+                return true;
+        return false;
+    }
+
+    private boolean inBox(int row, int col, int num) {
+        int r = row - row % 3;
+        int c = col - col % 3;
+        for (int i = r; i < r + 3; i++) {
+            for (int j = c; j < c + 3; j++) {
+                if (board[i][j] == num)
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isAccepted(int row, int col, int num) {
+        return !inRow(row, num) && !inCol(col, num) && !inBox(row, col, num);
+    }
+
+    public int[][] getBoard() {
+        return board;
+    }
+
+}
